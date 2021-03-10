d3d46ea12f143fc8eaf0f0fda5c176350455d529
Thomas Gößler <the____tiger@hotmail.com>
2021-03-10 22:50:55 +0100

    added wait handle extension

---
+++ Thread/WaitHandleExtension.cs
@@ -0,0 +1,46 @@
+﻿// *******************************************************************************
+// 
+//  *******   ***   ***               *
+//     *     *     *                  *
+//     *    *      *                *****
+//     *    *       ***  *   *   **   *    **    ***
+//     *    *          *  * *   *     *   ****  * * *
+//     *     *         *   *      *   * * *     * * *
+//     *      ***   ***    *     **   **   **   *   *
+//                         *
+// *******************************************************************************
+//  see https://github.com/ThE-TiGeR/TCSystemCS for details.
+//  Copyright (C) 2003 - 2021 Thomas Goessler. All Rights Reserved.
+// *******************************************************************************
+// 
+//  TCSystem is the legal property of its developers.
+//  Please refer to the COPYRIGHT file distributed with this source distribution.
+// 
+// *******************************************************************************
+
+#region Usings
+
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace TCSystem.Thread
+{
+    internal static class WaitHandleExt
+    {
+#region Public
+
+        public static async Task WaitOneAsync(this WaitHandle waitHandle)
+        {
+            await Task.Run(waitHandle.WaitOne);
+        }
+
+        public static async Task WaitOneAsync(this WaitHandle waitHandle, CancellationToken cancellationToken)
+        {
+            await Task.Run(waitHandle.WaitOne, cancellationToken);
+        }
+
+#endregion
+    }
+}
