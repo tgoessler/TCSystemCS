9e23b650374f43beeefe9bb42c829c759ed27f9b
Thomas Gößler <the____tiger@hotmail.com>
2021-04-06 22:19:49 +0200

    added async update helper extansion for getting a IDisposable

---
+++ Thread/AsyncUpdateHelperExt.cs
@@ -0,0 +1,48 @@
+﻿// *******************************************************************************
+// 
+//  *******   ***   ***               *
+//     *     *     *                  *
+//     *    *      *                *****
+//     *    *       ***  *   *   **   *    **    ***
+//     *    *          *  * *   *     *   ****  * * *
+//     *     *         *   *      *   * * *     * * *
+//     *      ***   ***    *     **   **   **   *   *
+//                         *
+// *******************************************************************************
+//  see https://github.com/ThE-TiGeR/TCSystemCS for details.
+//  Copyright (C) 2003 - 2021 Thomas Goessler. All Rights Reserved.
+// *******************************************************************************
+// 
+//  TCSystem is the legal property of its developers.
+//  Please refer to the COPYRIGHT file distributed with this source distribution.
+// 
+// *******************************************************************************
+
+#region Usings
+
+using System;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace TCSystem.Thread
+{
+    public static class AsyncUpdateHelperExt
+    {
+#region Public
+
+        public static async Task<IDisposable> BeginUpdateScopeAsync(this IAsyncUpdateHelper asyncUpdateHelper)
+        {
+            await asyncUpdateHelper.BeginUpdateAsync();
+            return new AsyncUpdateScope(asyncUpdateHelper);
+        }
+
+        public static async Task<IDisposable> WaitScopeAsync(this IAsyncUpdateHelper asyncUpdateHelper)
+        {
+            await asyncUpdateHelper.WaitAsync();
+            return new AsyncUpdateScope(asyncUpdateHelper);
+        }
+
+#endregion
+    }
+}
---
+++ Thread/AsyncUpdateScope.cs
@@ -0,0 +1,51 @@
+﻿// *******************************************************************************
+// 
+//  *******   ***   ***               *
+//     *     *     *                  *
+//     *    *      *                *****
+//     *    *       ***  *   *   **   *    **    ***
+//     *    *          *  * *   *     *   ****  * * *
+//     *     *         *   *      *   * * *     * * *
+//     *      ***   ***    *     **   **   **   *   *
+//                         *
+// *******************************************************************************
+//  see https://github.com/ThE-TiGeR/TCSystemCS for details.
+//  Copyright (C) 2003 - 2021 Thomas Goessler. All Rights Reserved.
+// *******************************************************************************
+// 
+//  TCSystem is the legal property of its developers.
+//  Please refer to the COPYRIGHT file distributed with this source distribution.
+// 
+// *******************************************************************************
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace TCSystem.Thread
+{
+    internal readonly struct AsyncUpdateScope : IDisposable
+    {
+#region Public
+
+        public AsyncUpdateScope(IAsyncUpdateHelper asyncUpdateHelper)
+        {
+            _asyncUpdateHelper = asyncUpdateHelper;
+        }
+
+        public void Dispose()
+        {
+            _asyncUpdateHelper.EndUpdate();
+        }
+
+#endregion
+
+#region Private
+
+        private readonly IAsyncUpdateHelper _asyncUpdateHelper;
+
+#endregion
+    }
+}
