757154b69fa33e2cc376e89fb7f1124a0e5dd19c
Thomas Gößler <the____tiger@hotmail.com>
2021-04-12 22:17:39 +0200

    added async extensions class

---
+++ Util/AsyncExt.cs
@@ -0,0 +1,45 @@
+﻿// *******************************************************************************
+// 
+//  *******   ***   ***               *
+//     *     *     *                  *
+//     *    *      *                *****
+//     *    *       ***  *   *   **   *    **    ***
+//     *    *          *  * *   *     *   ****  * * *
+//     *     *         *   *      *   * * *     * * *
+//     *      ***   ***    *     **   **   **   *   *
+//                         *
+// *******************************************************************************
+//  see https://github.com/ThE-TiGeR/TCSystemCS for details.
+//  Copyright (C) 2003 - 2021 Thomas Goessler. All Rights Reserved.
+// *******************************************************************************
+// 
+//  TCSystem is the legal property of its developers.
+//  Please refer to the COPYRIGHT file distributed with this source distribution.
+// 
+// *******************************************************************************
+
+#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace TCSystem.Util
+{
+    public static class AsyncExt
+    {
+#region Public
+
+        public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> action)
+        {
+            foreach (var item in source)
+            {
+                await action(item);
+            }
+        }
+
+#endregion
+    }
+}
