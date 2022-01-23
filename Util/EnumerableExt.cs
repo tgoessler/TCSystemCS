6bb1244fd2592f22a37167b1cd18790542d91d14
Thomas Gößler <the____tiger@hotmail.com>
2022-01-23 20:55:24 +0100

    added some utils

---
+++ Util/EnumerableExt.cs
@@ -0,0 +1,48 @@
+﻿// *******************************************************************************
+// 
+//  *******   ***   ***               *
+//     *     *     *                  *
+//     *    *      *                *****
+//     *    *       ***  *   *   **   *    **    ***
+//     *    *          *  * *   *     *   ****  * * *
+//     *     *         *   *      *   * * *     * * *
+//     *      ***   ***    *     **   **   **   *   *
+//                         *
+// *******************************************************************************
+//  see https://github.com/ThE-TiGeR/TCSystemCS for details.
+//  Copyright (C) 2003 - 2021 Thomas Goessler. All Rights Reserved.
+// *******************************************************************************
+// 
+//  TCSystem is the legal property of its developers.
+//  Please refer to the COPYRIGHT file distributed with this source distribution.
+// 
+// *******************************************************************************
+
+#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace TCSystem.Util
+{
+    public static class EnumerableExt
+    {
+#region Public
+
+        public static IEnumerable<TYpe> OrderRandom<TYpe>(this IEnumerable<TYpe> values)
+        {
+            return values.OrderBy(_ => _sRandom.Next());
+        }
+
+#endregion
+
+#region Private
+
+        private static readonly Random _sRandom = new Random();
+
+#endregion
+    }
+}
---
+++ Util/MathExt.cs
@@ -0,0 +1,45 @@
+﻿// *******************************************************************************
+// 
+//  *******   ***   ***               *
+//     *     *     *                  *
+//     *    *      *                *****
+//     *    *       ***  *   *   **   *    **    ***
+//     *    *          *  * *   *     *   ****  * * *
+//     *     *         *   *      *   * * *     * * *
+//     *      ***   ***    *     **   **   **   *   *
+//                         *
+// *******************************************************************************
+//  see https://github.com/ThE-TiGeR/TCSystemCS for details.
+//  Copyright (C) 2003 - 2021 Thomas Goessler. All Rights Reserved.
+// *******************************************************************************
+// 
+//  TCSystem is the legal property of its developers.
+//  Please refer to the COPYRIGHT file distributed with this source distribution.
+// 
+// *******************************************************************************
+
+namespace TCSystem.Util
+{
+    public static class MathExt
+    {
+#region Public
+
+        public static int ToNextPowerOf2(this int x)
+        {
+            if (x < 0)
+            {
+                return 0;
+            }
+
+            --x;
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            return x + 1;
+        }
+
+#endregion
+    }
+}
