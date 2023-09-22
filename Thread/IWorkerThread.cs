3c57f7c3200333eb57146f33ff53df8a77eb1a2e
Thomas Gößler <the____tiger@hotmail.com>
2023-09-22 22:35:30 +0200

    added events and cancelation token to worker thread

--- Thread/IWorkerThread.cs
+++ Thread/IWorkerThread.cs
@@ -21,6 +21,7 @@
 #region Usings
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 #endregion
@@ -39,10 +40,15 @@ namespace TCSystem.Thread
         Task ExecuteCommandAsync(Action action, string message);
         Task ClearOpenCommandsAsync();
 
-        void StopThread();
+        void StopThread(TimeSpan? timeOut=null);
 
         int NumOpenActions { get; }
         bool IsBusy { get; }
+        CancellationToken CancellationToken { get; }
+
+        event Action OnInitThread;
+        event Action OnDeInitThread;
+        event Action<bool> IdleEvent;
 
 #endregion
     }
